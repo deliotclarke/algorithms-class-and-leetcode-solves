58a9224d2a64290b3befb9ce6d74001949f48502
Daniel Clarke <deliotclarke@gmail.com>
2020-07-02 16:46:15 -0500

    completed anagram challenge. yeeeeee.

---
+++ algorithms-class/section-5-problem-solving-patterns/0-frequency-anagram-challenge.js
@@ -0,0 +1,55 @@
+/**
+ *
+ * Given two strings, write a function to determine if the second string is an anagram of the first.
+ * An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+ * No periods, numbers, punctuation or spelling. All lowercase.
+ *
+ *
+ * validAnagram('', '') // true
+ * validAnagram('aaz', 'zza') // false
+ * validAnagram('rat', 'car') // false
+ * validAnagram('qwerty', 'qeyrwt') // true
+ * validAnagram('awesome', 'awesom') // false
+ */
+
+function validAnagram(str1, str2) {
+  // compare that they are the same length thus can have equal frequencies
+  if (str1.length !== str2.length) return false;
+  if (str1.length === 0 && str2.length === 0) return true;
+
+  // declare objects/hashmaps
+  let letterCounter1 = {};
+  let letterCounter2 = {};
+
+  // build objects
+  for (let val in str1) {
+    let letter = str1[val];
+    letterCounter1[letter] = (letterCounter1[letter] || 0) + 1;
+  }
+  for (let val in str2) {
+    let letter = str2[val];
+    letterCounter2[letter] = (letterCounter2[letter] || 0) + 1;
+  }
+
+  // compare objects
+  for (let key in letterCounter1) {
+    // is each key in each object?
+    if (!(key in letterCounter2)) {
+      return false;
+    }
+    // does each key's value === the other's
+    if (letterCounter1[key] !== letterCounter2[key]) {
+      return false;
+    }
+  }
+  // if false isn't returned, return true
+  return true;
+}
+
+console.log(validAnagram('', ''));
+console.log(validAnagram('aaz', 'zza'));
+console.log(validAnagram('rat', 'car'));
+console.log(validAnagram('qwerty', 'qeyrwt'));
+console.log(validAnagram('iceman', 'cinema'));
+console.log(validAnagram('texttwisttime', 'timetwisttext'));
+console.log(validAnagram('bing', 'boing'));
