53618575e14872eb3ea544ad905288d3e3e66a2b
Daniel Clarke <deliotclarke@gmail.com>
2020-07-19 13:22:56 -0500

    finished longestString added a fun solve to areThereDupes

--- algorithms-class/section-6-bonus-challenges/findLongestSubstring.js
+++ algorithms-class/section-6-bonus-challenges/findLongestSubstring.js
@@ -3,53 +3,28 @@
  *
  */
 
-// ! This isn't finished!
+// Colt's solution so I can step through
 
 function findLongestSubstring(str) {
   if (str.length === 0) return 0;
+  let longest = 0;
   let set = {};
+  let start = 0;
 
-  const checkSet = (char) => {
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
     if (set[char]) {
-      set[char] = set[char] + 1;
-      return true;
-    } else {
-      set[char] = 1;
-      return false;
+      start = Math.max(start, set[char]);
     }
-  };
 
-  // create set
-  let fast = 0;
-  let slow = 0;
-  // ! if you ain't slow, you're fast
+    longest = Math.max(longest, i - start + 1);
 
-  // declare window length
-  let win = 1;
-
-  // TODO loop over string - not sure if should use slow or fast as end of loop
-  while (fast < str.length) {
-    // if a value doesn't exist in our set - add it
-    if (checkSet(str[fast])) {
-      // shrink window - moving slow up
-      set[str[slow]] = set[str[slow]] - 1;
-      slow++;
-      fast++;
-    } else if (set[str[fast]] === 1) {
-      // move start forward
-      fast++;
-      // increase our window size reflected in our window variable
-      // check max
-      win = Math.max(win, fast - slow);
-    } else {
-      fast++;
-    }
+    set[char] = i + 1;
   }
-
-  // win = Math.max(win, fast - slow) - calculate in the right place, after increase of end - should be able to get rid of + 1
-
-  return win;
+  return longest;
 }
 
 console.log(findLongestSubstring('codeitdude')); // 6
 console.log(findLongestSubstring('bbbbbb')); // 1
+
+// still having a hard time with this one. want to come back to it.
